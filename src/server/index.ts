535ca8abacdc7ecbe595317a76360b3010f06654
CuPa <p.cuje@campus.tu-berlin.de>
2021-05-07 13:09:53 +0200

    merged hmtl, added vue components

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,11 +6,11 @@ import TodoRouter from "./routes/todo";
 const app = express();
 const port = process.env.PORT || 80;
 
-app.use("/", express.static(__dirname + "/../public"));
+app.use("/", express.static(__dirname + "/../../public"));
 app.use("/todos", TodoRouter);
 
 app.get("/impressum", (_, res) =>
-  res.sendFile(path.join(__dirname + "/../public/impressum.html"))
+  res.sendFile(path.join(__dirname + "/../../public/impressum.html"))
 );
 
 app.listen(port, () => {
